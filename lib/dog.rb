381d494b2764dadfff7b2fbe7a6660e16612af96
Kulsum Shaikh <shaikhkulsum233@gmail.com>
2022-11-28 09:49:52 -0600

    commit tree de335262555cddc9cace4a58af6c621d166ee7c9

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,13 @@
-class Dog
+require "active_record"
+
+
+# ActiveRecord::Base.establish_connection(
+#     adapter: "sqlite3",
+#     database: "db/test.sqlite3"
+#   )
+class Dog < ActiveRecord::Base
+
+
+
+
 end
